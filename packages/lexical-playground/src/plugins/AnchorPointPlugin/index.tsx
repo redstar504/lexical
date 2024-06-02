1f4c8232152ab10659d0f47ec77144bcb663c20c
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-06-01 17:01:45 -0700

    add matchers setup

--- packages/lexical-playground/src/plugins/AnchorPointPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/AnchorPointPlugin/index.tsx
@@ -1,7 +1,15 @@
 import {
-  AnchorPointPlugin
+  AnchorPointPlugin, createAnchorPoint,
 } from '../../../../lexical-react/src/LexicalAnchorPointPlugin';
 
+const URL_REGEX = /((ftp|http|https):\/\/(.+:.+@)?)?([\w-/]+\.(?!$|\s))+\w*(:\d+)?([?/#][\w-~+?=#%&:/]*)?/
+
+const ANCHOR_POINTS = [
+  createAnchorPoint(URL_REGEX, text => {
+    return text.startsWith('http') ? text : `https://${text}`
+  })
+]
+
 export default function LexicalAnchorPointPlugin(): JSX.Element {
-  return <AnchorPointPlugin />
+  return <AnchorPointPlugin points={ANCHOR_POINTS} />
 }
--- packages/lexical-react/src/LexicalAnchorPointPlugin.tsx
+++ packages/lexical-react/src/LexicalAnchorPointPlugin.tsx
@@ -6,32 +6,120 @@
  *
  */
 
+import {$createAutoLinkNode, $isAutoLinkNode} from '@lexical/link';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import {$createTextNode, $isTextNode, LexicalEditor, TextNode} from 'lexical';
 import {useEffect} from 'react';
-import {LexicalEditor, TextNode} from 'lexical';
 
 // export const URL_REGEX = /https?:\/\/([\w-]+\.)*\w([\w-@?^=%&:/~+#]*)?/
 // export const URL_REGEX = /(ftp|http)s?:\/\/(.+:.+@)?([\w-]+\.(?!$|\s))*\w*[\w-@?^=%&:/~()+#]*(\.\w+)(?<=\/)/
 // export const URL_REGEX = /(ftp|http|https):\/\/(.+:.+@)?(([\w-]+\.(?!$|\s))|([\w-]+)+)+\w*[\w-~+?=#%&:/]*(\.\w+)*/
 // next one works good
 // export const URL_REGEX = /(ftp|http|https):\/\/(.+:.+@)?([\w-]+\.(?!$|\s))+\w*[\w-~+?=#%&:/]*(\.\w+)*/
+// export const URL_REGEX = /((ftp|http|https):\/\/(.+:.+@)?)?([\w-/]+\.(?!$|\s))+\w*(:\d+)?([?/#][\w-~+?=#%&:/]*)?/
 
-export const URL_REGEX = /(ftp|http|https):\/\/(.+:.+@)?([\w-/]+\.(?!$|\s))+\w*(:\d+)?([?/#][\w-~+?=#%&:/]*)?/
-
-function useAnchorPoint(editor: LexicalEditor): void {
-  useEffect(() => {
-    return editor.registerNodeTransform(TextNode, (textNode: TextNode) => {
-      // extract matches from within the text node
-
-
-    })
-  }, [editor]);
+type Anchor = {
+  index: number
+  length: number
+  text: string
+  url: string
 }
 
-export function AnchorPointPlugin() {
-  const [editor] = useLexicalComposerContext()
+export type AnchorPoint = (text: string) => Anchor
 
-  useAnchorPoint(editor)
+export function createAnchorPoint(
+  pattern: RegExp,
+  transform: (text: string) => string = (text) => text,
+) {
+  return (text: string) => {
+    const match = pattern.exec(text);
+    return match ? {
+      index: match.index,
+      length: match[0].length,
+      text: match[0],
+      url: transform(match[0]),
+    } : null;
+  };
+}
 
-  return null
+export function findNextAnchor(
+  text: string,
+  points: AnchorPoint[],
+): Anchor | null {
+  for (let i = 0; i < points.length; i++) {
+    const anchor = points[i](text);
+    if (anchor) {
+      return anchor;
+    }
+  }
+
+  return null;
+}
+
+function useAnchorPoint(editor: LexicalEditor, points: AnchorPoint[]): void {
+  useEffect(() => {
+    return editor.registerNodeTransform(TextNode, (textNode: TextNode) => {
+      let currentNode = textNode;
+      const parent = textNode.getParentOrThrow();
+      const prevSibling = textNode.getPreviousSibling();
+
+      // extract matches from within the text node
+      if (!textNode.isSimpleText() || $isAutoLinkNode(parent)) {
+        return;
+      }
+
+      // Handle appending additional matching URL text to a previous link node
+      if (prevSibling && $isAutoLinkNode(prevSibling)) {
+        const prevText = prevSibling.getTextContent();
+        const nodeText = textNode.getTextContent();
+        const combinedText = prevText + nodeText;
+        const rematch = findNextAnchor(combinedText, points);
+
+        if (!rematch) {
+          return;
+        }
+
+        const prevLinkTextNode = prevSibling.getFirstChild();
+
+        if (!$isTextNode(prevLinkTextNode) || !prevLinkTextNode.isSimpleText()) {
+          return;
+        }
+
+        // append to the autolink node, and subtract from the text node for each additional match
+        const numCharsToShift = rematch.length - prevText.length;
+        textNode.setTextContent(nodeText.substring(numCharsToShift));
+        prevLinkTextNode.setTextContent(rematch.text);
+        prevSibling.setURL(rematch.url);
+
+        // reset the selection to the end of the shifted text node
+      }
+
+      const nodeText = textNode.getTextContent();
+      const anchor = findNextAnchor(nodeText, points);
+
+      if (!anchor) {
+        return;
+      }
+
+      const matchOffset = anchor.index;
+      const matchLength = anchor.length;
+      let matchedNode;
+
+      if (matchOffset === 0) {
+        [matchedNode, currentNode] = currentNode.splitText(matchLength);
+      } else {
+        [, matchedNode, currentNode] = currentNode.splitText(matchOffset, matchOffset + matchLength);
+      }
+
+      const linkNode = $createAutoLinkNode(anchor.url);
+      linkNode.append($createTextNode(anchor.text));
+      matchedNode.replace(linkNode);
+    });
+  }, [editor, points]);
+}
+
+export function AnchorPointPlugin({points}: {points: AnchorPoint[]}) {
+  const [editor] = useLexicalComposerContext();
+  useAnchorPoint(editor, points);
+  return null;
 }
